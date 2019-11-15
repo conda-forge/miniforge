051b305444c287d675d517100992214501d27065
Hadrien Mary <hadrien.mary@gmail.com>
2019-11-14 23:21:07 -0500

    Add LICENSE to build/

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ set -ex
 cd /construct
 
 cp -R Miniforge3 build/Miniforge3/
+cp LICENSE build/
 
 echo "version: $(git describe)" >> build/Miniforge3/construct.yaml
 constructor build/Miniforge3/ --output-dir build/
+
 rm -fr build/Miniforge3/
+rm -f build/LICENSE
