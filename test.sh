d5399c5ad5e3031f69c75cd3cc317963349dc119
Hadrien Mary <hadrien.mary@gmail.com>
2019-11-14 22:31:25 -0500

    rename conda to miniforge

--- test.sh
+++ test.sh
@@ -7,14 +7,14 @@ cd /construct
 INSTALLER_PATH=$(find build/ -name 'Miniforge*.sh' | head -n 1)
 
 chmod +x $INSTALLER_PATH
-bash $INSTALLER_PATH -b -p ${HOME}/conda
+bash $INSTALLER_PATH -b -p ${HOME}/miniforge
 
-__conda_setup="$($HOME'/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$($HOME'/miniforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f $HOME/conda/etc/profile.d/conda.sh ]; then
-        . $HOME/conda/etc/profile.d/conda.sh
+    if [ -f $HOME/miniforge/etc/profile.d/conda.sh ]; then
+        . $HOME/miniforge/etc/profile.d/conda.sh
     else
         export PATH=$HOME/conda/bin:$PATH
     fi
