82e4d6c3d3c9dcce229dd6585cfdf3b9234c2688
Hadrien Mary <hadrien.mary@gmail.com>
2019-11-16 13:03:18 -0500

    remove set -x and add echo during build and test

--- build.sh
+++ build.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+set -e
 
 # Constructor should be >= 3.0.1 for aarch64.
 # See https://github.com/conda-forge/miniforge/pull/2#issuecomment-554394343
+echo "* Install constructor"
 conda install -y constructor>=3.0.1
 conda list
 
+echo "* Make temp directory"
 TEMP_DIR=$(mktemp -d)
 
 cd /construct
 
-# Copy constructor spec to $TEMP_DIR
+echo "* Copy file for installer construction"
 cp -R Miniforge3/ $TEMP_DIR/
 cp LICENSE $TEMP_DIR
 
-# Set the version of the installer from git
+echo "* Set installer version"
 echo "version: $(git describe)" >> $TEMP_DIR/Miniforge3/construct.yaml
 
-# Build the installer
+echo "* Construct the installer"
 constructor $TEMP_DIR/Miniforge3/ --output-dir $TEMP_DIR
 
-# Move installer to build/
+echo "* Move installer to build/"
 mv $TEMP_DIR/Miniforge*.sh build/
--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
-# Install dependencies
+export CONDA_PATH="$HOME/miniforge"
+
+echo "* Install dependencies"
 if [ -f /etc/redhat-release ]; then
   yum install -y bzip2
 fi
@@ -13,35 +15,26 @@ fi
 
 cd /construct
 
-# Get the first installer we find.
-# TODO: be smarter here maybe?
+echo "* Get the installer"
 INSTALLER_PATH=$(find build/ -name "Miniforge*$ARCH.sh" | head -n 1)
 
-# Run the installer
+echo "* Run the installer"
 chmod +x $INSTALLER_PATH
-bash $INSTALLER_PATH -b -p ${HOME}/miniforge
+bash $INSTALLER_PATH -b -p $CONDA_PATH
 
-# Enable conda
-__conda_setup="$($HOME'/miniforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f $HOME/miniforge/etc/profile.d/conda.sh ]; then
-        . $HOME/miniforge/etc/profile.d/conda.sh
-    else
-        export PATH=$HOME/miniforge/bin:$PATH
-    fi
-fi
-unset __conda_setup
+echo "* Setup conda"
+$CONDA_PATH/bin/conda 'shell.bash' 'hook' 2> /dev/null
 
-# Run some tests to check everything is fine.
-
+echo "* Print conda info"
 conda info
 
-# Check if conda update/install works.
+echo "* Run conda update"
 conda update --all -y
 
+echo "* Python path"
 which python
+
+echo "* Print system informations from Python"
 python -c "print('Hello Miniforge !')"
 python -c "import platform; print(platform.architecture())"
 python -c "import platform; print(platform.system())"
