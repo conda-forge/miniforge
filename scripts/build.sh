2431366d27785b22000ae977e10b7b08c615ebf4
Hadrien Mary <hadrien.mary@gmail.com>
2019-11-18 15:13:53 -0500

    define conda version in construct.yaml

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,17 +4,11 @@ set -e
 
 cd /construct
 
-CONDA_VERSION=$(git tag --points-at HEAD)
-CONDA_VERSION="4.7.11-0"
-if [ -z "$CONDA_VERSION" ]
-then
-  echo "***** No Conda version detected in git tag.*****"
-else
-  CONDA_VERSION=$(echo $CONDA_VERSION | cut -d "-" -f 1)
-  echo "***** Conda version detected in git tag: $CONDA_VERSION *****"
-  echo "Install appropriate conda version."
-  conda install -y "conda=$CONDA_VERSION"
-fi
+MINIFORGE_VERSION=$(cat Miniforge/construct.yaml | grep version | cut -d ":" -f 2)
+CONDA_VERSION=$(echo $MINIFORGE_VERSION | cut -d "-" -f 1)
+
+echo "***** Install conda version: $CONDA_VERSION *****"
+conda install -y "conda=$CONDA_VERSION"
 
 # Constructor should be >= 3.0.1 for aarch64.
 # See https://github.com/conda-forge/miniforge/pull/2#issuecomment-554394343
@@ -28,9 +22,6 @@ TEMP_DIR=$(mktemp -d)
 echo "***** Copy file for installer construction *****"
 cp -R Miniforge/ $TEMP_DIR/
 cp LICENSE $TEMP_DIR
-
-echo "***** Set installer version *****"
-echo "version: $(git describe)" >> $TEMP_DIR/Miniforge/construct.yaml
 
 echo "***** Construct the installer *****"
 constructor $TEMP_DIR/Miniforge/ --output-dir $TEMP_DIR
