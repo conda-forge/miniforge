2a4e80d4fd6f10dd32c2e5662149e8f20ecd3f3e
YuviPanda <yuvipanda@gmail.com>
2020-03-08 13:40:58 +0530

    Fix clean-home test for non-root users

--- tests/tests/clean-homedir.py
+++ tests/tests/clean-homedir.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import os
 
-# Only .bashrc and .profile should be in $HOME
-assert sorted(os.listdir(os.environ['HOME'])) == ['.bashrc', '.profile']
+# .bash_logout could optionally be in $HOME, otherwise only .bashrc and .profile
+home_contents = sorted(os.listdir(os.environ['HOME']))
+if len(home_contents) == 3:
+    assert home_contents == ['.bash_logout', '.bashrc', '.profile'], home_contents
+else:
+    assert home_contents == ['.bashrc', '.profile'], home_contents
