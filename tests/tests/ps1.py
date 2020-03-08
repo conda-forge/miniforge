2b2275efbd6941fdb7267c90b600d277448e0c83
YuviPanda <yuvipanda@gmail.com>
2020-03-08 13:33:49 +0530

    Add tests as non-root as well

--- tests/tests/ps1.py
+++ tests/tests/ps1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import subprocess
+import os
 
 # Interactive mode in bash should have (base) in PS1
 # -i requires stdin to be a tty, but we don't care. stderr is silenced to keep output clean.
