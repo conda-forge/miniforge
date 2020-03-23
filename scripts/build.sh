fdfb31d6a0fd16ad789ef7e624aa5610a274aeca
Isuru Fernando <isuruf@gmail.com>
2020-03-23 13:22:59 -0500

    Install jinja2

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ cd $CONSTRUCT_ROOT
 # Constructor should be >= 3.0.1 for aarch64.
 # See https://github.com/conda-forge/miniforge/pull/2#issuecomment-554394343
 echo "***** Install constructor *****"
-conda install -y "constructor>=3.0.1"
+conda install -y "constructor>=3.0.1" jinja2
 pip install git+git://github.com/conda/constructor@5c9539a95ed7eafb#egg=constructor --force --no-deps
 conda list
 
