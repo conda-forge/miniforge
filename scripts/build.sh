3899b9d32be3ce3f27c1e3abe5b7be6323f474af
Isuru Fernando <isuruf@gmail.com>
2020-11-10 21:17:15 -0600

    fix channels

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,11 +11,11 @@ cd $CONSTRUCT_ROOT
 # Constructor should be latest for non-native building
 # See https://github.com/conda/constructor
 echo "***** Install constructor *****"
-conda install -y "constructor>=3.1.0" jinja2
+conda install -y "constructor>=3.1.0" jinja2 -c conda-forge -c defaults --override-channels
 if [[ "$(uname)" == "Darwin" ]]; then
-    conda install -y coreutils
+    conda install -y coreutils -c conda-forge -c defaults --override-channels
 elif [[ "$(uname)" == "MSYS"* ]]; then
-    conda install -y "nsis=3.01"
+    conda install -y "nsis=3.01" -c conda-forge -c defaults --override-channels
 fi
 pip install git+git://github.com/conda/constructor@8c0121d3b81846de42973b52f13135f0ffeaddda#egg=constructor --force --no-deps
 conda list
