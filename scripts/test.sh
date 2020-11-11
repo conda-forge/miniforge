c3b9ca92f59be81bccff93e9f6a7efdb05a35b17
Isuru Fernando <isuruf@gmail.com>
2020-11-10 23:25:41 -0600

    Check if we are bundling packages from msys2 or defaults

--- scripts/test.sh
+++ scripts/test.sh
@@ -21,10 +21,14 @@ if [[ "$(uname)" == MINGW* ]]; then
 
   echo "***** Setup conda *****"
   source $CONDA_PATH/Scripts/activate
+  conda.exe config --set show_channel_urls true
 
   echo "***** Print conda info *****"
   conda.exe info
-  conda.exe list
+
+  echo "***** Check if we are bundling packages from msys2 or defaults *****"
+  conda.exe list | grep defaults && exit 1
+  conda.exe list | grep msys2 && exit 1
 else
   bash $INSTALLER_PATH -b -p $CONDA_PATH
 
