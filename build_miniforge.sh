05172ef877cdf606ba16ebb4775085a4fe10f133
romainx <romainx@users.noreply.github.com>
2020-12-18 17:42:01 +0100

    Use qemu-user-static in persistent mode
    Avoid unnecessary download of qemu and maintenance of the get_qemu.sh script.

--- build_miniforge.sh
+++ build_miniforge.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-
+# Build miniforge installers for various architectures
+# It uses the qemu-user-static [1] emulator to enable 
+# the use of containers images with different architectures than the host
+# [1]: https://github.com/multiarch/qemu-user-static/
 set -ex
 
 # Check parameters
 ARCH=${ARCH:-aarch64}
-DOCKER_ARCH=${DOCKER_ARCH:arm64v8}
+DOCKER_ARCH=${DOCKER_ARCH:-arm64v8}
 DOCKERIMAGE=${DOCKERIMAGE:-condaforge/linux-anvil-aarch64}
 export CONSTRUCT_ROOT=/construct
 
@@ -13,18 +16,14 @@ mkdir -p build/
 chmod 777 build/
 
 echo "============= Enable QEMU ============="
-docker run --rm --privileged multiarch/qemu-user-static:register --reset --credential yes
+# Enable qemu in persistent mode
+docker run --rm --privileged multiarch/qemu-user-static --reset --credential yes --persistent yes
 
 echo "============= Build the installer ============="
 docker run --rm -v $(pwd):/construct -e CONSTRUCT_ROOT -e MINIFORGE_VERSION -e MINIFORGE_NAME $DOCKERIMAGE /construct/scripts/build.sh
 
-echo "============= Download QEMU static binaries ============="
-bash scripts/get_qemu.sh
-
 echo "============= Test the installer ============="
-for TEST_IMAGE_NAME in "ubuntu:20.04" "ubuntu:19.10" "ubuntu:16.04" "ubuntu:18.04" "centos:7" "debian:buster"
-do
+for TEST_IMAGE_NAME in "ubuntu:20.04" "ubuntu:19.10" "ubuntu:16.04" "ubuntu:18.04" "centos:7" "debian:buster"; do
   echo "============= Test installer on $TEST_IMAGE_NAME ============="
-  docker run --rm -v $(pwd):/construct -e CONSTRUCT_ROOT -v $(pwd)/build/qemu/qemu-${ARCH}-static:/usr/bin/qemu-${ARCH}-static ${DOCKER_ARCH}/$TEST_IMAGE_NAME /construct/scripts/test.sh
+  docker run --rm -v $(pwd):/construct -e CONSTRUCT_ROOT ${DOCKER_ARCH}/$TEST_IMAGE_NAME /construct/scripts/test.sh
 done
-
