c3279688cd23dd3f08da9f98a52ce8ba30db5028
romainx <romainx@users.noreply.github.com>
2020-12-21 21:34:41 +0100

    Remove get_qemu.sh
    Also quoted some variables.

--- build_miniforge.sh
+++ build_miniforge.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-# Build miniforge installers for various architectures
+# Build miniforge installers for Linux 
+# on various architectures (aarch64, x86_64, ppc64le)
+# Notes:
 # It uses the qemu-user-static [1] emulator to enable 
 # the use of containers images with different architectures than the host
 # [1]: https://github.com/multiarch/qemu-user-static/
+# See also: [setup-qemu-action](https://github.com/docker/setup-qemu-action)
 set -ex
 
 # Check parameters
@@ -17,13 +20,17 @@ chmod 777 build/
 
 echo "============= Enable QEMU ============="
 # Enable qemu in persistent mode
-docker run --rm --privileged multiarch/qemu-user-static --reset --credential yes --persistent yes
+docker run --rm --privileged multiarch/qemu-user-static \
+  --reset --credential yes --persistent yes
 
 echo "============= Build the installer ============="
-docker run --rm -v $(pwd):/construct -e CONSTRUCT_ROOT -e MINIFORGE_VERSION -e MINIFORGE_NAME $DOCKERIMAGE /construct/scripts/build.sh
+docker run --rm -v "$(pwd):/construct" \
+  -e CONSTRUCT_ROOT -e MINIFORGE_VERSION -e MINIFORGE_NAME \
+  ${DOCKERIMAGE} /construct/scripts/build.sh
 
 echo "============= Test the installer ============="
 for TEST_IMAGE_NAME in "ubuntu:20.04" "ubuntu:19.10" "ubuntu:16.04" "ubuntu:18.04" "centos:7" "debian:buster"; do
-  echo "============= Test installer on $TEST_IMAGE_NAME ============="
-  docker run --rm -v $(pwd):/construct -e CONSTRUCT_ROOT ${DOCKER_ARCH}/$TEST_IMAGE_NAME /construct/scripts/test.sh
+  echo "============= Test installer on ${TEST_IMAGE_NAME} ============="
+  docker run --rm -v "$(pwd):/construct" -e CONSTRUCT_ROOT \
+    "${DOCKER_ARCH}/${TEST_IMAGE_NAME}" /construct/scripts/test.sh
 done
