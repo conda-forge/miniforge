c63fb6d448c8e4c911452268f950ee8615d6e853
Isuru Fernando <isuruf@gmail.com>
2021-02-10 21:20:55 -0600

    Update build_miniforge.sh

--- build_miniforge.sh
+++ build_miniforge.sh
@@ -28,6 +28,9 @@ docker run --rm -v "$(pwd):/construct" \
   -e CONSTRUCT_ROOT -e MINIFORGE_VERSION -e MINIFORGE_NAME -e TARGET_PLATFORM \
   ${DOCKERIMAGE} /construct/scripts/build.sh
 
+# copy the installer for latest
+cp build/$MINIFORGE_NAME-*-$OS_NAME-$ARCH.$EXT build/$MINIFORGE_NAME-$OS_NAME-$ARCH.$EXT
+
 echo "============= Test the installer ============="
 for TEST_IMAGE_NAME in "ubuntu:20.04" "ubuntu:19.10" "ubuntu:16.04" "ubuntu:18.04" "centos:7" "debian:buster"; do
   echo "============= Test installer on ${TEST_IMAGE_NAME} ============="
