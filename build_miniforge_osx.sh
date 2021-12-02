ea2e0e93c5e73f7c0d62602b58daba1ef39464c4
Mark Harfouche <mark.harfouche@gmail.com>
2021-12-01 19:07:04 -0500

    Keep in sync with miniforge images

--- build_miniforge_osx.sh
+++ build_miniforge_osx.sh
@@ -4,15 +4,26 @@ set -e
 set -x
 
 echo "Installing a fresh version of Miniforge3."
-MINIFORGE_VERSION="4.10.3-10"
-MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}"
-MINIFORGE_FILE="Mambaforge-${MINIFORGE_VERSION}-MacOSX-x86_64.sh"
-curl -L -O "${MINIFORGE_URL}/${MINIFORGE_FILE}"
-bash "${MINIFORGE_FILE}" -b
+# Keep variable names in sync with 
+# https://github.com/conda-forge/docker-images/blob/main/scripts/run_commands
+miniforge_arch="$(uname -m)"
+miniforge_version="4.10.3-10"
+condapkg="https://github.com/conda-forge/miniforge/releases/download/${miniforge_version}/Mambaforge-${miniforge_version}-MacOSX-${miniforge_arch}.sh"
+if [ "$(uname -m)" = "x86_64" ]; then
+   conda_chksum="7c44259a0982cd3ef212649678af5f0dd4e0bb7306e8fffc93601dd1d739ec0b"
+elif [ "$(uname -m)" = "arm64" ]; then
+   conda_chksum="72bc86612ab9435915b616c2edb076737cbabe2c33fd684d58c2f9ae72e1957c"
+else
+   exit 1
+fi
+curl -s -L $condapkg > miniconda.sh
+sha256sum miniconda.sh | grep $conda_chksum
+
+bash miniconda.sh -b -p ~/conda
 
 echo "Configuring conda."
 # shellcheck disable=SC1090
-source ~/mambaforge/bin/activate root
+source ~/conda/bin/activate root
 
 export CONSTRUCT_ROOT="${PWD}"
 mkdir -p build
