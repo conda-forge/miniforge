bbd9d5a17f3e21831c52b38a55aa122b88435dc3
Mark Harfouche <mark.harfouche@gmail.com>
2022-01-08 02:49:47 -0500

    Update constructor pinnings

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,9 +12,7 @@ cd "${CONSTRUCT_ROOT}"
 # Constructor should be latest for non-native building
 # See https://github.com/conda/constructor
 echo "***** Install constructor *****"
-# pyyaml 6 broke constructor
-# https://github.com/conda/constructor/pull/473
-conda install -y "constructor>=3.1.0" "pyyaml<6" jinja2 curl libarchive -c conda-forge --override-channels
+conda install -y "constructor>=3.2.2" jinja2 curl libarchive -c conda-forge --override-channels
 
 
 if [[ "$(uname)" == "Darwin" ]]; then
