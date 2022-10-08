43db77ad66567fcd7b39ed827c93e0d3c9e8aad5
Mark Harfouche <mark.harfouche@gmail.com>
2022-10-08 11:53:27 -0400

    Update OSX miniforge version

--- build_miniforge_osx.sh
+++ build_miniforge_osx.sh
@@ -4,20 +4,20 @@ set -e
 set -x
 
 echo "Installing a fresh version of Miniforge3."
-# Keep variable names in sync with 
+# Keep variable names in sync with
 # https://github.com/conda-forge/docker-images/blob/main/scripts/run_commands
 miniforge_arch="$(uname -m)"
-miniforge_version="4.10.3-10"
+miniforge_version="4.14.0-0"
 condapkg="https://github.com/conda-forge/miniforge/releases/download/${miniforge_version}/Mambaforge-${miniforge_version}-MacOSX-${miniforge_arch}.sh"
 if [ "$(uname -m)" = "x86_64" ]; then
-   conda_chksum="7c44259a0982cd3ef212649678af5f0dd4e0bb7306e8fffc93601dd1d739ec0b"
+   conda_checksum="949f046b4404cc8e081807b048050e6642d8db5520c20d5158a7ef721fbf76c5"
 elif [ "$(uname -m)" = "arm64" ]; then
-   conda_chksum="72bc86612ab9435915b616c2edb076737cbabe2c33fd684d58c2f9ae72e1957c"
+   conda_checksum="35d05a65e19b8e5d596964936ddd6023ae66d664a25ba291a52fec18f06a73b6"
 else
    exit 1
 fi
-curl -s -L "$condapkg" > miniconda.sh
-openssl sha256 miniconda.sh | grep $conda_chksum
+curl -s -L "$condapkg" -o miniconda.sh
+openssl sha256 miniconda.sh | grep $conda_checksum
 
 bash miniconda.sh -b -p ~/conda
 
