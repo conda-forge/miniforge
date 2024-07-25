80a37aad03af0db26e99edad6267da5ec1ce7282
jaimergp <jaimergp@users.noreply.github.com>
2024-07-25 09:01:57 +0200

    Address code review

--- Miniforge3/mambaforge_deprecation.bat
+++ Miniforge3/mambaforge_deprecation.bat
@@ -1,9 +1,8 @@
-#!/bin/bash
-
-if "$GITHUB_ACTIONS"=="1" (
+if "%GITHUB_ACTIONS%"=="true" (
     echo ::warning title=Mambaforge is now deprecated!::Future Miniforge releases will NOT build Mambaforge installers. We advise you switch to Miniforge at your earliest convenience.
 )
 else (
     msg "%sessionname%" Mambaforge is now deprecated! Future Miniforge releases will NOT build Mambaforge installers. We advise you switch to Miniforge at your earliest convenience.
-    timeout /t 30 /nobreak
 )
+echo Sleeping for 30s...
+powershell -nop -c "& {sleep 30}"
