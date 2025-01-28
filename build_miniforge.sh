6512f45de3929ec80f24bad27ecfad8e57522924
Julien Jerphanion <git@jjerphan.xyz>
2025-01-28 11:26:44 +0100

    Do not mount trust store
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- build_miniforge.sh
+++ build_miniforge.sh
@@ -37,6 +37,5 @@ for TEST_IMAGE_NAME in ${TEST_IMAGE_NAMES}; do
   echo "============= Test installer on ${TEST_IMAGE_NAME} ============="
   docker run --rm \
     -v "$(pwd):${CONSTRUCT_ROOT}" -e CONSTRUCT_ROOT \
-    -v /etc/ssl/certs:/etc/ssl/certs:ro \
     --platform "linux/${DOCKER_ARCH}" "${DOCKER_ARCH/\//}/${TEST_IMAGE_NAME}" /construct/scripts/test.sh
 done
