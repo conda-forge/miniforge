ab2e73c97576ed7fa2383ac077dc5e94c391a4a3
jaimergp <jaimergp@users.noreply.github.com>
2026-05-11 10:32:34 +0200

    Sign EXE installers on Windows (#874)

---
+++ scripts/verify_signatures_and_checksums.ps1
@@ -0,0 +1,33 @@
+param (
+    [string]$BuildDir = "build"
+)
+# NOTE: 'param' block MUST always be the first thing on file.
+
+$ErrorActionPreference = "Stop"  # Exit immediately on most errors
+
+$exitCode = 0
+$exeFiles = Get-ChildItem "$BuildDir/*.exe"
+
+# Verifies signatures for previously signed EXE files in the build/ directory
+
+foreach ($executable in $exeFiles) {
+    Write-Host "******************************************"
+    Write-Host $executable.Name
+    Write-Host "******************************************"
+    $sig = Get-AuthenticodeSignature -FilePath $executable.FullName
+    $sig | Format-List * | Out-String | Write-Host
+    if ($sig.Status -ne 'Valid') {
+        Write-Error "CRITICAL: Signature verification failed for $($executable.Name)!"
+        $exitCode = 1
+    }
+}
+
+# Regenerate the SHA256 files, since the checksum changes with the signature
+
+foreach ($executable in $exeFiles) {
+    $hashObject = Get-FileHash $executable.FullName
+    "$($hashObject.Hash.ToLower())  $($executable.Name)" | Out-File -FilePath "$($executable.FullName).sha256"
+    Write-Host "SHA256($($executable.Name)): $($hashObject.Hash.ToLower())"
+}
+
+exit $exitCode
